b3cbaef353ac5a27625b22c5cf48b5008d34c675
Al Mahfuz <al.mahfuz@selise.ch>
2021-02-27 11:58:03 +0600

    normally work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,73 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
+
+
 export class AppComponent implements AfterViewInit {
-
-  ngOnInit(){
-    console.log("I'm in web view ng onit");
-  }
-  ngAfterViewInit() {
-
-    (window as any).addEventListener("ns-bridge-ready", function(e) {
-
-
-      console.log("I'm in web ngAfterViewInit");
-      (window as any).nsWebViewBridge.on('webReceiverEvent', function(msg: any) {
-          // alert(JSON.stringify(msg.data));
-          console.log("I'm in web view");
-          alert("good to go");
-  });
-
- });
-}
-  title = 'native-wrapper';
-  initiateScannerEvent() {
-    //alert("I've been clicked!")
-
-    var nsWebViewBridge = (window as any).nsWebViewBridge;
-    nsWebViewBridge.emit('scannerEvent', { trigger: true });
-}
+    constructor(
+        private cdrf: ChangeDetectorRef){
+    }
+    clicked = true;
+    ngOnInit() {
+        console.log("I'm in web view ng onit");
+    }
+    ngAfterViewInit() {
+        // it works for current version
+        // window.addEventListener('load', (event) => {
+        //     setTimeout(() => {
+        //         (window as any).nsWebViewBridge.on('webReceiverEvent', (data) => {
+        //             alert("back in web");
+        //             this.toggleClick();
+        //             console.log('markers event from native'); console.dir(data);
+        //         });
+        //     }, 100);
+        // });
+        //its the previous code
+        // window.addEventListener("ns-bridge-ready", function (e) {
+        //     setTimeout(() => {
+        //         (window as any).nsWebViewBridge.on('webReceiverEvent', function (msg: any) {
+        //             // alert(JSON.stringify(msg.data));
+        //             alert("good to go");
+        //         });
+        //     }, 1000);
+        // });
+        window.addEventListener('load', ()=> {
+            setTimeout(() => {
+                (window as any).nsWebViewBridge.on('webReceiverEvent',  ((msg: any)=> {
+                    this.title = 'changed native';
+                    this.toggleClick();
+                    alert(JSON.stringify(msg.message)); 
+                }));
+            }, 1000); 
+        });
+    }
+    title = 'native-wrapper';
+    initiateScannerEvent() {
+        //this.toggleClick();
+        var nsWebViewBridge = (window as any).nsWebViewBridge;
+        nsWebViewBridge.emit('scannerEvent', { trigger: true });
+    }
+    anotherfunction(){
+        alert("I'm in another function");
+        this.toggleClick();
+    }
+    toggleClick(){
+        debugger
+        console.log("I'm in toggle function 1" +this.clicked);
+        if(this.clicked){
+            this.clicked = false;
+        } else {
+            this.clicked = true;
+        }
+        this.cdrf.detectChanges();
+        this.cdrf.markForCheck();
+        //alert("I'm in toggle function");
+        console.log("I'm in toggle function 2" + this.clicked);
+    }
 }
 
